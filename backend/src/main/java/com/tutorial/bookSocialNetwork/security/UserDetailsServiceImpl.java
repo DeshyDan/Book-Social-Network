90f2b661281986e90181db55ee7bff7627a458b6
Deshy Dan <deshy733@gmail.com>
2024-06-20 23:29:02 +0200

    Add user details service implementation

---
+++ backend/src/main/java/com/tutorial/bookSocialNetwork/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.tutorial.bookSocialNetwork.security;
+
+import com.tutorial.bookSocialNetwork.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return repository.findByEmail(userName)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+}
