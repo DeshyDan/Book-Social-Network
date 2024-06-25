257ee4cc8918d56d49f86a01e17f407898ed0f78
Deshy Dan <deshy733@gmail.com>
2024-06-25 06:14:48 +0200

    Add customer auditing

--- backend/src/main/java/com/tutorial/bookSocialNetwork/config/BeansConfig.java
+++ backend/src/main/java/com/tutorial/bookSocialNetwork/config/BeansConfig.java
@@ -3,6 +3,7 @@ package com.tutorial.bookSocialNetwork.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -34,5 +35,10 @@ public class BeansConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 }
