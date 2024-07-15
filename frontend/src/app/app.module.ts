23368f6a375e8f2a24f865a0c80c7714008a625f
Deshy Dan <deshy733@gmail.com>
2024-07-15 21:25:22 +0200

    Migrate frontend IAM to keycloak

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -11,6 +11,13 @@ import {HttpTokenInterceptor} from './services/interceptor/http-token.intercepto
 import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from 'angular-code-input';
 import {ApiModule} from "./services/api.module";
+import {KeycloakService} from "./services/keycloak/keycloak.service";
+
+
+export function kcFactory(kcService: KeycloakService) {
+  return ()=> kcService.init();
+
+}
 
 @NgModule({
   declarations: [
@@ -25,7 +32,7 @@ import {ApiModule} from "./services/api.module";
     FormsModule,
     HttpClientModule,
     CodeInputModule,
-    ApiModule.forRoot({rootUrl:'http:'})
+    ApiModule.forRoot({rootUrl: 'http:'})
   ],
   providers: [
     HttpClient,
@@ -33,7 +40,12 @@ import {ApiModule} from "./services/api.module";
       provide: HTTP_INTERCEPTORS,
       useClass: HttpTokenInterceptor,
       multi: true
-    },
+    }, {
+      provide: APP_INITIALIZER,
+      deps: [KeycloakService],
+      useFactory: kcFactory,
+      multi: true
+    }
 
   ],
   bootstrap: [AppComponent]
